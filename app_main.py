d2236591857e532cda29b934d891a63db8ca1cdf
Nguyễn Xuân Trúc <105005372+nxtruc@users.noreply.github.com>
2025-03-16 16:45:25 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -2,19 +2,16 @@ import streamlit as st
 import Classification 
 import Clustering_Algorithms 
 import Linear_Regression
-import Semi_supervised
 import NN
 import PCA_t_sne 
 
 st.set_page_config(page_title="Machine Learning App", layout="wide")
 
-menu = ["Semi-supervised","Classification", "Clustering", "Linear Regression", "PCA/t-SNE", "Neural network"]
+menu = ["Classification", "Clustering", "Linear Regression", "PCA/t-SNE", "Neural network"]
 choice = st.sidebar.selectbox("Chọn chức năng", menu)
 
 
-if choice == "Semi-supervised":
-    Semi_supervised.run()
-elif choice == "Classification":
+if choice == "Classification":
     Classification.run()
 elif choice == "Clustering":
     Clustering_Algorithms.run()
